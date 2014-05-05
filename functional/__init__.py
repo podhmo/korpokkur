5cd1d2aae195666f70702e11ab1d5d3ab370f6f3
podhmo <podhmo+altair@beproud.jp>
2014-05-05 17:34:44 +0900

    tiny functional test

---
+++ functional/__init__.py
@@ -0,0 +1,27 @@
+# -*- coding:utf-8 -*-
+import os.path
+import sys
+from mako_scaffold.command import main
+
+def err(s):
+    sys.stderr.write(s)
+    sys.stderr.write("\n")
+
+def cleanup():
+    err("cleanup ..")
+    if os.path.exists(os.path.join(here, "build")):
+        import shutil
+        shutil.rmtree(os.path.join(here, "build"))
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+cleanup()
+main("_ create --logging DEBUG --config {here}/foo.ini simple-package {here}/build".format(here=here).split(" "))
+
+is_exists = lambda x : os.path.join(here,"build", x)
+
+assert (is_exists("foo"))
+assert (is_exists("foo/setup.py"))
+
+print("ok")
+
