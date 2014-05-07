90ad69c74701a26af9eb388edaf46d14a38fc7d3
podhmo <podhmo+altair@beproud.jp>
2014-05-07 17:42:28 +0900

    forget manifest.ini

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup, find_packages
 requires = [
     "mako", 
-    "zope.interface"
+    "zope.interface", 
     ]
 
+testing_extras = [
+    "jinja2", 
+]
+
 setup(name='korpokkur',
-      version='0.0.1',
+      version='0.0.2',
       description='scaffold',
       long_description="", 
       author='podhmo',
@@ -16,7 +20,11 @@ setup(name='korpokkur',
       package_dir={'': '.'},
       packages=find_packages('.'),
       install_requires = requires,
-      test_suite="korpokkur.tests", 
+      extras_require = {
+          'testing':testing_extras,
+          },
+      include_package_data=True, 
+      test_suite='korpokkur.tests',
       entry_points = """
       [console_scripts]
       korpokkur = korpokkur.command:main
