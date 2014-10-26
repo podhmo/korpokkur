6bf0aac62f46188ad878c49048e480ffd8f6aff9
podhmo <podhmo+altair@beproud.jp>
2014-10-26 22:03:48 +0900

    tweak

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 pep8:
 	(cd tmp && korpokkur create --config ../sample.json "package[pytest]")
 	(cd tmp && korpokkur create --config ../sample2.json "nestedpackage[pytest]")
+	(cd tmp && korpokkur create --config ../sample3.json "scaffold")
 	find tmp -name "*.py" | xargs pep8 || echo "package"
--- korpokkur/scaffolds/package/__init__.py
+++ korpokkur/scaffolds/package/__init__.py
@@ -4,17 +4,18 @@ from zope.interface import implementer
 from korpokkur.interfaces import IScaffoldTemplate
 import os.path
 
-## see: korpokkur.interfaces:IScaffoldTemplate
+
+# see: korpokkur.interfaces:IScaffoldTemplate
 @implementer(IScaffoldTemplate)
 class Package(object):
     """sample python package scaffold extensions=[nose, pytest]"""
     source_directory = os.path.join(os.path.abspath(os.path.dirname(__file__)), "+package+")
-    ##varname -> description, default
+    # varname -> description, default
     expected_words = {
-        "package": ("package name", "sample"), 
-        "description": ("package description", "-"), 
+        "package": ("package name", "sample"),
+        "description": ("package description", "-"),
         "version": ("version number for project", "0.0")
     }
-    __dro__ = ["korpokkur.scaffolds.pygitignore:Package"] #todo:validation
+    __dro__ = ["korpokkur.scaffolds.pygitignore:Package"]  # todo:validation
     template_engine = "mako"
-    support_extensions = ["unittest", "nose", "pytest"] #default is unittest
+    support_extensions = ["unittest", "nose", "pytest"]  # default is unittest
