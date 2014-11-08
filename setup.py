1eca53c89248e8b751828a352f31a0d17fb88ef4
podhmo <podhmo+altair@beproud.jp>
2014-11-08 20:58:56 +0900

    bump version tiny

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ testing_extras = [
 ]
 
 setup(name='korpokkur',
-      version='0.0.8',
+      version='0.0.8.1',
       url="https://github.com/podhmo/korpokkur", 
       description='scaffolding',
       long_description="", 
