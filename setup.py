742da1c190630e50783dfbf6cd49cc3b3e1a1275
podhmo <podhmo+altair@beproud.jp>
2015-03-08 23:15:53 +0900

    bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ testing_extras = [
 ]
 
 setup(name='korpokkur',
-      version='0.0.8.1',
+      version='0.0.9',
       url="https://github.com/podhmo/korpokkur", 
       description='scaffolding',
       long_description="", 
